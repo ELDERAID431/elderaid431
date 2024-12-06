66ca9df2d4523e81f82f30b9205946c4d99c2faa
selen <slnkprmci01@gmail.com>
2024-12-06 22:41:53 +0300

    2 commit

--- app/src/main/java/com/example/elderaid/MainActivity.kt
+++ app/src/main/java/com/example/elderaid/MainActivity.kt
@@ -3,45 +3,20 @@ package com.example.elderaid
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.elderaid.ui.theme.ElderAidTheme
+import androidx.navigation.compose.rememberNavController
+import com.example.elderaid.ui.navigation.AppNavHost
+import com.example.elderaid.ui.theme.ElderaidTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            ElderAidTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            ElderaidTheme {
+                val navController = rememberNavController()
+                AppNavHost(navController = navController, startDestination = "login")
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ElderAidTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/example/elderaid/ui/screens/SignupScreen.kt
+++ app/src/main/java/com/example/elderaid/ui/screens/SignupScreen.kt
@@ -8,7 +8,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.elderaid.viewmodel.SignupViewModel
+import com.example.elderaid.ui.viewmodel.SignupViewModel
 
 @Composable
 fun SignupScreen(viewModel: SignupViewModel = viewModel(), onSignupSuccess: () -> Unit) {
