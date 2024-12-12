6a274d2d5a5140c99976ab95440e004b9da4931b
selen <slnkprmci01@gmail.com>
2024-12-12 16:01:21 +0300

    Selen NavHost.kt ve MainActivity düzenlenmesi

--- app/src/main/java/com/example/elderaid/MainActivity.kt
+++ app/src/main/java/com/example/elderaid/MainActivity.kt
@@ -3,12 +3,9 @@ package com.example.elderaid
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.example.elderaid.ui.navigation.AppNavHost
 import com.example.elderaid.ui.theme.ElderAidTheme
-
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,7 +13,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ElderAidTheme {
                 val navController = rememberNavController()
-                AppNavHost(navController = navController, startDestination = "login")
+                AppNavHost(navController = navController, startDestination = "getstarted")
             }
         }
     }
--- app/src/main/java/com/example/elderaid/ui/navigation/NavHost.kt
+++ app/src/main/java/com/example/elderaid/ui/navigation/NavHost.kt
@@ -4,18 +4,27 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.example.elderaid.ui.screens.GetStartedScreen
 import com.example.elderaid.ui.screens.LoginScreen
 import com.example.elderaid.ui.screens.SignupScreen
 
 @Composable
 fun AppNavHost(navController: NavHostController, startDestination: String) {
     NavHost(navController = navController, startDestination = startDestination) {
+        // Get Started Screen
+        composable("getstarted") {
+            GetStartedScreen(navController = navController)
+        }
+
+        // Login Screen
         composable("login") {
             LoginScreen(
                 onSignupClick = { navController.navigate("signup") },
                 onLoginSuccess = { /* Başarı durumu */ }
             )
         }
+
+        // Signup Screen
         composable("signup") {
             SignupScreen(
                 onSignupSuccess = { navController.navigate("login") }
